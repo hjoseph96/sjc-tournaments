c10e1e7691654e47f43a35c3f5e83ff386d3d512
aguxez <diazmiiguel@gmail.com>
2018-06-20 13:31:05 -0400

    adds more helper functions

--- lib/sjc/models.ex
+++ lib/sjc/models.ex
@@ -6,13 +6,54 @@ defmodule Sjc.Models do
   alias __MODULE__.{Tournament}
   alias Sjc.Repo
 
+  @doc "Returns all tournaments"
   def all_tournaments do
     Repo.all(Tournament)
   end
 
+  @doc """
+  Gets a tournament by id.
+
+  Raises 'Ecto.NoResultsError' if the Tournament doesn't exist.
+  """
+  def get_tournament!(id) do
+    Repo.get!(Tournament, id)
+  end
+
+  @doc """
+  Creates a new tournament.
+  Returns {:ok, %Tournament{}} | {:error, %Ecto.Changeset{}}
+  """
   def create_tournament(%{} = args) do
     %Tournament{}
     |> Tournament.changeset(args)
     |> Repo.insert()
+  end
+
+  @doc """
+  Updates a tournament.
+
+  Returns {:ok, %Tournament{}} | {:error, %Ecto.Changeset{}}
+  """
+  def update_tournament(%Tournament{} = tournament, attrs) do
+    tournament
+    |> Tournament.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Deletes a tournament.
+
+  Returns {:ok, %Tournament{}} | {:error, %Ecto.Changeset{}}
+  """
+  def delete_tournament(%Tournament{} = tournament) do
+    Repo.delete(tournament)
+  end
+
+  @doc """
+  Returns an %Ecto.Changeset{} for tracking tournament changes.
+  """
+  def change_tournament(%Tournament{} = tournament) do
+    Tournament.changeset(tournament, %{})
   end
 end
