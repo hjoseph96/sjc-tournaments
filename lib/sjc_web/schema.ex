42881bd023f3d0356e6ce006424270e2525d6981
aguxez <diazmiiguel@gmail.com>
2018-06-21 18:18:48 -0400

    Adds basic schema for Game GenServers

--- lib/sjc_web/schema.ex
+++ lib/sjc_web/schema.ex
@@ -3,5 +3,46 @@ defmodule SjcWeb.Schema do
   Schema for GraphQL.
   """
 
-  # use Absinthe.Schema
+  use Absinthe.Schema
+
+  alias SjcWeb.GameResolver
+
+  object :round_obj do
+    field :number, :integer
+  end
+
+  object :game do
+    field :name, :string
+    field :round, :round_obj
+    # field :players, :array
+    # field :actions, :array
+  end
+
+  query do
+    field :get_game, :game do
+      arg :name, non_null(:string)
+
+      resolve &GameResolver.get_game/3
+    end
+
+    field :stop_game, :game do
+      arg :name, non_null(:string)
+
+      resolve &GameResolver.stop_game/3
+    end
+
+    field :next_round, :game do
+      arg :name, non_null(:string)
+
+      resolve &GameResolver.next_round/3
+    end
+  end
+
+  mutation do
+    field :create_game, :game do
+      arg :name, non_null(:string)
+
+      resolve &GameResolver.create_game/3
+    end
+  end
 end
