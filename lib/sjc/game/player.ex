4b67e63c5d618d81ddd3f858b39059df679dabdf
aguxez <diazmiiguel@gmail.com>
2018-06-22 14:28:38 -0400

    adds id and renames module

---
+++ lib/sjc/game/player.ex
@@ -0,0 +1,7 @@
+defmodule Sjc.Game.Player do
+  @moduledoc """
+  Module that provides a player struct for player stats.
+  """
+
+  defstruct ~w(id health_points shield_points accuracy luck)a
+end
--- lib/sjc/game/players.ex
+++
@@ -1,7 +0,0 @@
-defmodule Sjc.Game.Players do
-  @moduledoc """
-  Module that provides a player struct for player stats.
-  """
-
-  defstruct ~w(health_points shield_points accuracy luck)a
-end
