bf8c637a1d095ab33c627548bb3f2662a895d5b9
aguxez <diazmiiguel@gmail.com>
2018-06-22 11:48:55 -0400

    fixes error with module name causing struct to fail at creation

--- test/sjc/game/players_test.exs
+++ test/sjc/game/players_test.exs
@@ -1,4 +1,4 @@
-defmodule Sjc.Game.Players do
+defmodule Sjc.Game.PlayersTest do
   @moduledoc false
 
   use ExUnit.Case, async: true
--- test/sjc/game_test.exs
+++ test/sjc/game_test.exs
@@ -37,11 +37,13 @@ defmodule Sjc.GameTest do
         shield_points: 100.0
       }
 
-      assert {:ok, "added"} = Game.add_player("game_1", attributes)
+      assert {:ok, :added} = Game.add_player("game_1", attributes)
       assert length(Game.state("game_1").players) == 1
     end
 
     test "returns {:error, already added} when player is a duplicate" do
+      GameSupervisor.start_child("game_5")
+
       attributes = %{
         accuracy: 5.2,
         health_points: 100.0,
@@ -49,8 +51,8 @@ defmodule Sjc.GameTest do
         shield_points: 100.0
       }
 
-      assert {:ok, "added"} = Game.add_player("game_1", attributes)
-      assert {:error, "already added"} = Game.add_player("game_1", attributes)
+      assert {:ok, :added} = Game.add_player("game_5", attributes)
+      assert {:error, :already_added} = Game.add_player("game_5", attributes)
     end
   end
 end
