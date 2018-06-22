e19a0832173952df6a8589d6f505a4f50d90e555
aguxez <diazmiiguel@gmail.com>
2018-06-22 11:02:07 -0400

    adds player struct

---
+++ lib/sjc/game/players.ex
@@ -0,0 +1,7 @@
+defmodule Sjc.Game.Players do
+  @moduledoc """
+  Module that provides a player struct for player stats.
+  """
+
+  defstruct ~w(health_points shield_points accuracy luck)a
+end
---
+++ test/sjc/game/players_test.exs
@@ -0,0 +1,5 @@
+defmodule Sjc.Game.Players do
+  @moduledoc false
+
+  use ExUnit.Case, async: true
+end
