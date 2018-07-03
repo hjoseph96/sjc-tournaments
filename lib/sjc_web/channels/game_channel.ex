dc5683952b42f5dc6b8e16fc8f3a85cd0034ce3a
Miguel Diaz <diazmiiguel@gmail.com>
2018-07-02 23:04:49 -0400

    adds support for game channel and socket

---
+++ lib/sjc_web/channels/game_channel.ex
@@ -0,0 +1,22 @@
+defmodule SjcWeb.GameChannel do
+  @moduledoc """
+  Top module to manage connections to the game channel through the websocket.
+  """
+
+  use Phoenix.Channel
+
+  alias Sjc.Supervisors.GameSupervisor
+
+  def join("game:" <> game_name, _message, socket) do
+    {:ok, pid} = GameSupervisor.start_child(game_name)
+
+    {:ok, assign(socket, :game_pid, pid)}
+  end
+
+  def handle_in("stop_game", %{"game" => game_identifier}, socket) do
+    # This function returns the pid of the stopped process.
+    pid = GameSupervisor.stop_child(game_identifier)
+
+    {:reply, {:ok, %{"stopped" => pid}}, socket}
+  end
+end
