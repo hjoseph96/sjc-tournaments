790e5376ac6ccb2659b03c0dd7d27e74a4194a1f
Miguel Diaz <diazmiiguel@gmail.com>
2018-07-09 20:02:38 -0400

    handles requests for time left

--- lib/sjc_web/channels/game_channel.ex
+++ lib/sjc_web/channels/game_channel.ex
@@ -6,12 +6,24 @@ defmodule SjcWeb.GameChannel do
   use Phoenix.Channel
 
 
-
-  # alias Sjc.Supervisors.GameSupervisor
+  alias Sjc.Game
 
   # TODO: Don't create a game, instead check for 'create_game' messages
   # and create OR join the player to an existing game.
   def join("game:" <> _game_name, _message, socket) do
     {:ok, socket}
+  end
+
+  def handle_in("next_round", %{}, socket) do
+    broadcast!(socket, "next_round", %{})
+
+    {:noreply, socket}
+  end
+
+  def handle_in("time_left", %{"game" => game_name}, socket) do
+    time = Game.time_of_round_left(game_name)
+    push(socket, "time_left", %{time: time})
+
+    {:noreply, socket}
   end
 end
