3cff7340aa4b34643cd3aa82059089b1afb0456a
Miguel Diaz <diazmiiguel@gmail.com>
2018-07-13 03:06:05 -0400

    Informs parent of changes in component

--- front-demo/src/components/Player.js
+++ front-demo/src/components/Player.js
@@ -4,8 +4,37 @@ export default class Player extends Component {
   constructor(props) {
     super(props);
 
-    console.log(this.props);
+    this.state = {
+      type: "damage",
+      amount: 0.0,
+      to: 0,
+      from: 0,
+      index: 0
+    }
+
+    this._handleChange = this._handleChange.bind(this);
+    this._handleTargetChange = this._handleTargetChange.bind(this);
+    this._getRandom = this._getRandom.bind(this);
   }
+
+  componentDidMount() {
+    this.setState({ to: this.props.player.id, from: this.props.player.id, index: this.props.index })
+  }
+
+  _handleChange(e) {
+    let amount = this._getRandom([14.4, 13.6, 32.1, 35.2, 26.4]);
+    this.setState({ type: e.target.value, amount: amount });
+
+    this.props.callbackParent(this.state);
+  }
+
+  _handleTargetChange(e) {
+    this.setState({ to: e.target.value })
+
+    this.props.callbackParent(this.state);
+  }
+
+  _getRandom(arr) { return arr[Math.floor(Math.random * arr.length)] }
 
   render() {
     return(
@@ -15,6 +44,23 @@ export default class Player extends Component {
         <td>{this.props.player.shieldPoints}</td>
         <td>{this.props.player.luck}</td>
         <td>{this.props.player.accuracy}</td>
+
+        <td>
+          <select value={this.state.type} onChange={this._handleChange}>
+            <option value="damage">Bomb</option>
+            <option value="shield">Shield</option>
+          </select>
+        </td>
+
+        <td>
+          <select value={this.state.to} onChange={this._handleTargetChange}>
+            {
+              this.props.players.map(player => (
+                <option key={player.id} value={player.id}>{player.id}</option>
+              ))
+            }
+          </select>
+        </td>
       </tr>
     )
   }
